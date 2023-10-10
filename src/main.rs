965a793418c4ca15de9838dde462740f8d7d58cb
chomnr <zeljkovranjes@outlook.com>
2023-10-09 21:44:55 -0400

    added support for enormous numbers.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::{io::{BufRead, self, Write}, vec, time::Duration, thread::sleep};
+use core::num;
+use std::{io::{self, Write}};
 
+use num_bigint::BigUint;
 
 fn main() {
     
@@ -16,14 +18,24 @@ fn main() {
 
     // collatz conjecture
     let mut collatz = CollatzSequence::new(parse);
+    let mut collatz_v2 = CollatzEnormousSequence::new(parse);
+
 
     // keep looping until last value == 1;
+    /* 
     while collatz.seq[collatz.i] != 1 {
         // sequence the collatz conjecture...
         collatz.sequence();
+    }*/
+
+    while collatz_v2.seq[collatz_v2.i] != BigUint::from(1 as usize) {
+        // sequence the collatz conjecture...
+        collatz_v2.sequence();
     }
 }
 
+
+// Sequence smaller numbers
 struct CollatzSequence {
     i: usize,
     seq: Vec<u128>
@@ -38,16 +50,53 @@ impl CollatzSequence {
         let v =  &self.seq[self.i];
         let mut r = 0;
 
-        if self.seq[0] % 2 == 0 {
+        if v % 2 == 0 {
             let r1 = v/2; // even rule
             r = r1;
         } else {
             let r2 = 3*(v)+1; // odd rule
             r = r2;
         }
+
         self.seq.push(r);
         self.i += 1;
 
         println!("Result: {}", r);
+    }
+}
+
+struct CollatzEnormousSequence {
+    i: usize,
+    seq: Vec<BigUint>
+}
+
+impl CollatzEnormousSequence {
+    fn new(start: u128) -> Self {
+        CollatzEnormousSequence {
+            i: 0,
+            seq: vec![BigUint::from(start)]
+        }
+    }
+
+    fn sequence(&mut self) {
+        let v = &self.seq[self.i];
+        let mut r = self.b(0);
+
+        if v % self.b(2) == self.b(0) {
+            let r1 = v/self.b(2); // even rule
+            r = r1;
+        } else {
+            let r2 = self.b(3)*(v)+self.b(1); // odd rule
+            r = r2;
+        }
+
+        self.seq.push(r.clone());
+        self.i += 1;
+
+        println!("[BIG] Result: {:?}", r);
+    }
+    
+    fn b(&self, number: usize) -> BigUint {
+        return BigUint::from(number)
     }
 }
